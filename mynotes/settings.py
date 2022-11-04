ee2dd0355e915569565ac0c6a784c9ab2ad83d39
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-11-04 16:54:38 +0000

    add api

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*%x701c9ec1qaj&5h_15pyoji2bfb#xg4ks_eppjig+8ggjrt9"
+SECRET_KEY = "django-insecure-m$f24h=vtjxe+@!69(37sy*8)w!)zba4c%7rll6goc!=ka273j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
