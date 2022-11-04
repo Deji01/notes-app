c3f11c1dc3c274304c64ca49256d94f78c2690ed
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-11-04 18:21:32 +0000

    set up react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Headers from './components/Headers';
+
+function App() {
+  return (
+    <div className="App">
+      <Headers/>
+      MY APP
+    </div>
+  );
+}
+
+export default App;
